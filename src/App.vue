9d4db17d3f61ea34a6f2894ae35015e9bcae78a0
tazirahmb <tazirahmb@outlook.com>
2018-12-12 16:47:28 +0700

    add TodoList

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TodoList,
   },
 };
 </script>
